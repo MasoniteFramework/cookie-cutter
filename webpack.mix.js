cc7ecb139160e1afbf84c413f86d1018b5df398c
Junior Gantin <nioperas06@gmail.com>
2020-02-01 22:20:43 +0100

    Update webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -6,7 +6,7 @@ let mix = require('laravel-mix');
  |--------------------------------------------------------------------------
  |
  | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
+ | for your Masonite application. By default, we are compiling the Sass
  | file for your application, as well as bundling up your JS files.
  |
  */
