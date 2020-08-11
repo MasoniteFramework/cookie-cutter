cd62f12409fd5c61981bab2c5441029ae687d8ee
Joe Mancuso <idmann509@gmail.com>
2020-08-11 19:10:14 -0400

    removed orator imports and replaced with Masonite ORM

--- config/database.py
+++ config/database.py
@@ -1,66 +1,76 @@
-"""Database Settings."""
+""" Database Settings """
 
-import logging
+import os
 
-from masonite import env
-from masonite.environment import LoadEnvironment
-from orator import DatabaseManager, Model
+from masonite.environment import LoadEnvironment, env
+from masonite.orm.builder import QueryBuilder
 
-"""Load Environment Variables
-Loads in the environment variables when this page is imported.
+"""
+|--------------------------------------------------------------------------
+| Load Environment Variables
+|--------------------------------------------------------------------------
+|
+| Loads in the environment variables when this page is imported.
+|
 """
 
 LoadEnvironment()
 
-"""Database Settings
-Set connection database settings here as a dictionary. Follow the
-format below to create additional connection settings.
-
-Each key is a connection, not a driver. You may have as many
-connections as you need.
-
-Supported Drivers: 'sqlite', 'mysql', 'postgres'
+"""
+The connections here don't determine the database but determine the "connection".
+They can be named whatever you want.
 """
 
 DATABASES = {
-    'default': env('DB_CONNECTION'),
-    'sqlite': {
-        'driver': 'sqlite',
-        'database': env('DB_DATABASE'),
-        'log_queries': env('DB_LOG'),
-    },
+    'default': 'mysql',
     'mysql': {
         'driver': 'mysql',
-        'host': env('DB_HOST'),
-        'database': env('DB_DATABASE'),
-        'port': env('DB_PORT'),
-        'user': env('DB_USERNAME'),
-        'password': env('DB_PASSWORD'),
-        'log_queries': env('DB_LOG'),
+        'host': 'localhost',
+        'user': 'root',
+        'password': '',
+        'database': 'orm',
+        'port': '3306',
+        'prefix': '',
+        'grammar': 'mysql',
+        'options': {
+            'charset': 'utf8mb4',
+        },
     },
     'postgres': {
         'driver': 'postgres',
-        'host': env('DB_HOST'),
-        'database': env('DB_DATABASE'),
-        'port': env('DB_PORT'),
-        'user': env('DB_USERNAME'),
-        'password': env('DB_PASSWORD'),
-        'log_queries': env('DB_LOG'),
+        'host': 'localhost',
+        'user': 'postgres',
+        'password': 'postgres',
+        'database': 'orm',
+        'port': '5432',
+        'prefix': '',
+        'grammar': 'postgres',
     },
+    'sqlite': {
+        'driver': 'sqlite',
+        'database': 'orm.sqlite3',
+        'prefix': ''
+    }
 }
 
-DB = DatabaseManager(DATABASES)
-Model.set_connection_resolver(DB)
+# DB = QueryBuilder(connection_details=DATABASES)
 
+# DATABASES = {
+#     'default': os.environ.get('DB_DRIVER'),
+#     'sqlite': {
+#         'driver': 'sqlite',
+#         'database': os.environ.get('DB_DATABASE')
+#     },
+#     'postgres': {
+#         'driver': 'postgres',
+#         'host': env('DB_HOST'),
+#         'database': env('DB_DATABASE'),
+#         'port': env('DB_PORT'),
+#         'user': env('DB_USERNAME'),
+#         'password': env('DB_PASSWORD'),
+#         'log_queries': env('DB_LOG'),
+#     },
+# }
 
-logger = logging.getLogger('orator.connection.queries')
-logger.setLevel(logging.DEBUG)
-
-formatter = logging.Formatter(
-    'It took %(elapsed_time)sms to execute the query %(query)s'
-)
-
-handler = logging.StreamHandler()
-handler.setFormatter(formatter)
-
-logger.addHandler(handler)
+# DB = DatabaseManager(DATABASES)
+# Model.set_connection_resolver(DB)
