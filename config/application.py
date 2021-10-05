6b9d47c2623a8da2fd32a80cbdd7a625dc1d916b
Joe Mancuso <idmann509@gmail.com>
2021-10-05 17:02:37 -0400

    improved app config

--- config/application.py
+++ config/application.py
@@ -1,9 +1,10 @@
 import os
+from masonite.environment import env
 
 KEY = os.getenv("APP_KEY", "-RkDOqXojJIlsF_I8wWiUq_KRZ0PtGWTOZ676u5HtLg=")
 
 HASHING = {
-    "default": "bcrypt",
+    "default": env("HASHING_FUNCTION", "bcrypt"),
     "bcrypt": {"rounds": 10},
     "argon2": {"memory": 1024, "threads": 2, "time": 2},
 }
