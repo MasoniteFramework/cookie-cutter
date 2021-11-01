528de7549baf9a6fd1192b8c8dadbd700cb80de9
Samuel Girardin <samuelgirardin@pm.me>
2021-11-01 11:42:53 +0100

    add new AuthorizationProvider

--- config/providers.py
+++ config/providers.py
@@ -13,6 +13,7 @@ from masonite.providers import (
     HelpersProvider,
     BroadcastProvider,
     AuthenticationProvider,
+    AuthorizationProvider,
     HashServiceProvider,
 )
 
@@ -41,5 +42,6 @@ PROVIDERS = [
     HashServiceProvider,
     AuthenticationProvider,
     ValidationProvider,
+    AuthorizationProvider,
     ORMProvider
 ]
