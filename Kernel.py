d254ca97674ad2ad6ce1c3b5a284b0f96406a611
Samuel Girardin <samuelgirardin@pm.me>
2021-12-21 22:04:06 +0100

    add observers default location

--- Kernel.py
+++ Kernel.py
@@ -61,6 +61,7 @@ class Kernel:
         self.application.bind("events.location", "app/events")
         self.application.bind("tasks.location", "app/tasks")
         self.application.bind("models.location", "app/models")
+        self.application.bind("observers.location", "app/models/observers")
 
         self.application.bind("server.runner", "masonite.commands.ServeCommand.main")
 
