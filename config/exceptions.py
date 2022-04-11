cc347e0a36c43742780943a2b501f40b8569a6ee
Samuel Girardin <samuelgirardin@pm.me>
2022-04-11 10:09:56 +0200

    Update `exceptions` config for new error handler

--- config/exceptions.py
+++ config/exceptions.py
@@ -1,1 +1,23 @@
-HANDLERS = {"stack_overflow": True, "solutions": True}
+OPTIONS = {
+    "editor": "vscode",
+    "search_url": "https://www.google.com/search?q=",
+    "links": {
+        "doc": "https://docs.masoniteproject.com",
+        "repo": "https://github.com/MasoniteFramework/masonite",
+    },
+    "stack": {"offset": 10, "shorten": True},
+    "hide_sensitive_data": True,
+}
+
+HANDLERS = {
+    "dumps": True,
+    "solutions": {
+        "stackoverflow": False,
+        "possible_solutions": True
+    },
+    "recommendations": {
+        "packages_updates": {
+            "list": ["exceptionite", "masonite", "masonite-orm", "pytest"]
+        }
+    },
+}
