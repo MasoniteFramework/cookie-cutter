a319bcee1ac85feb7748b1ce588fac9cb612983c
Samuel Girardin <samuelgirardin@pm.me>
2022-04-12 19:57:03 +0200

    add missing api config

---
+++ config/api.py
@@ -0,0 +1,13 @@
+from app.models.User import User
+
+
+DRIVERS = {
+    "jwt": {
+        "algorithm": "HS512",
+        "secret": "secret",
+        "model": User,
+        "expires": None,
+        "authenticates": False,
+        "version": None,
+    }
+}
