504c30c739e2f86f81265b485e04f2afad447616
Samuel Girardin <samuelgirardin@pm.me>
2022-05-17 11:40:15 +0200

    Add new CORS config

---
+++ config/cors.py
@@ -0,0 +1,15 @@
+"""Cross-Origin Resource Sharing (CORS) config"""
+
+PATHS = ["api/*"]
+
+ALLOWED_METHODS = ["*"]
+
+ALLOWED_ORIGINS = ["*"]
+
+ALLOWED_HEADERS = ["*"]
+
+EXPOSED_HEADERS = []
+
+MAX_AGE = None
+
+SUPPORTS_CREDENTIALS = False
